6c47da74959033f263cdefc3dec9ba22eaa7f3fa
hatlonely <hatlonely@gmail.com>
2019-08-20 19:53:55 +0800

    update

--- cmd/cloud/main.go
+++ cmd/cloud/main.go
@@ -1,8 +1,81 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "github.com/hpifu/go-kit/logger"
+    "github.com/spf13/viper"
+    "os"
+)
+
+// AppVersion name
+var AppVersion = "unknown"
 
 func main() {
-    fmt.Println("hello world")
-}
+    version := flag.Bool("v", false, "print current version")
+    configfile := flag.String("c", "configs/account.json", "config file path")
+    flag.Parse()
+    if *version {
+        fmt.Println(AppVersion)
+        os.Exit(0)
+    }
 
+    // load config
+    config := viper.New()
+    config.SetConfigType("json")
+    fp, err := os.Open(*configfile)
+    if err != nil {
+        panic(err)
+    }
+    err = config.ReadConfig(fp)
+    if err != nil {
+        panic(err)
+    }
+
+    // init logger
+    infoLog, err := logger.NewTextLoggerWithViper(config.Sub("logger.infoLog"))
+    if err != nil {
+        panic(err)
+    }
+    warnLog, err := logger.NewTextLoggerWithViper(config.Sub("logger.warnLog"))
+    if err != nil {
+        panic(err)
+    }
+    accessLog, err := logger.NewJsonLoggerWithViper(config.Sub("logger.accessLog"))
+    if err != nil {
+        panic(err)
+    }
+    _ = warnLog
+    _ = accessLog
+
+    // init gin
+    gin.SetMode(gin.ReleaseMode)
+    r := gin.New()
+    r.Use(gin.Recovery())
+    r.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:4000")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    })
+
+    // set handler
+    r.GET("/ping", func(ctx *gin.Context) {
+        ctx.String(200, "ok")
+    })
+
+    infoLog.Infof("%v init success, port [%v]", os.Args[0], config.GetString("service.port"))
+
+    // run server
+    if err := r.Run(config.GetString("service.port")); err != nil {
+        panic(err)
+    }
+}
